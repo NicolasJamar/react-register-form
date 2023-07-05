9a079db17ec8f744aa2515c9f3b1b81124a9ce4f
NicolasJamar <nicolas.jamar@gmail.com>
2023-07-05 22:47:52 +0200

    add PersistLogin to protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Unauthorized from './components/Unauthorized';
 import Lounge from './components/Lounge';
 import LinkPage from './components/LinkPage';
 import RequireAuth from './components/RequireAuth';
+import PersistLogin from './components/PersistLogin';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
@@ -30,17 +31,19 @@ function App() {
         <Route path="unauthorized" element={<Unauthorized />} />
 
         {/* we want to protect these routes */}
-        <Route element={<RequireAuth allowedRoles={[ROLES.User]}/>}> 
-          <Route path="/" element={<Home />} />
-        </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Editor]}/>}> 
-          <Route path="editor" element={<Editor />} />
-        </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Admin]}/>}> 
-          <Route path="admin" element={<Admin />} />
-        </Route>
-        <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor]}/>}> 
-          <Route path="lounge" element={<Lounge />} />
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth allowedRoles={[ROLES.User]}/>}> 
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Editor]}/>}> 
+            <Route path="editor" element={<Editor />} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin]}/>}> 
+            <Route path="admin" element={<Admin />} />
+          </Route>
+          <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.Editor]}/>}> 
+            <Route path="lounge" element={<Lounge />} />
+          </Route>
         </Route>
 
         {/* catch all */}
